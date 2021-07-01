90c27b2ad0e78f0ac22365ae76a02781f7a459d7
Domino731 <79329862+Domino731@users.noreply.github.com>
2021-07-01 16:55:36 +0200

    rwd - breakpoint 600px

---
+++ src/js/menu.js
@@ -0,0 +1,23 @@
+// this script is responsible for showing or hiding the menu on mobile devices
+
+// menu icon for event
+const menuIcon = document.querySelector("#menu__icon")
+
+// menu
+const menu = document.querySelector(".header__nav")
+
+// all menu links
+const menuLinks = document.querySelectorAll(".nav__li")
+
+// show or hide menu
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("fa-times-circle")
+    menu.classList.toggle("active")
+})
+
+// when user click the element from menu, then hide this menu
+menuLinks.forEach(el => {
+    el.addEventListener("click", () => {
+        menu.classList.toggle("active")
+    })
+})
